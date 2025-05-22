6db683faf69a057a9313ec89df274d816f05ef87
Rohit <rdg922@gmail.com>
2025-05-22 03:02:54 -0700

    fix moving to postgres

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,29 +1,16 @@
-import { Pool } from "pg";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
 
 import { env } from "~/env";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  client: postgres.Sql | undefined;
-};
-
-
-// Create postgres client using neon for pooled connections
-const client = globalForDb.client ?? postgres(env.DATABASE_URL, { 
-  max: 10,
-  idle_timeout: 20,
-  connect_timeout: 10,
-  ssl: {
-    rejectUnauthorized: true,
-  },
+const client = new Client({
+  connectionString: env.DATABASE_URL,
 });
 
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+await client.connect();
+export const db = drizzle(client, {
+  schema: schema
+});
 
-export const db = drizzle(client, { schema });
+// TODO: add back in prod additions
